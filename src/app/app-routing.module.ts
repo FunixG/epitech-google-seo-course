3344421d751c1bc8c08cce394b13341d9a0c7f40
Antoine PRONNIER (Funix) <antoine.pronnier@gmail.com>
2023-01-14 08:18:17 +0100

    First version with all lorem ipsum created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./features/home/home.component";
+import {AboutComponent} from "./features/about/about.component";
+import {ContactComponent} from "./features/contact/contact.component";
+import {ProductComponent} from "./features/product/product.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ContactComponent } from './features/contact/contact.component';
+import { ProductComponent } from './features/product/product.component';
+import { NavbarComponent } from './features/navbar/navbar.component';
+import { FooterComponent } from './features/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ProductComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
